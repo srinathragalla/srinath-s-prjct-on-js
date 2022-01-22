99915a214e568091038d957aae24bc6353a3d61c
srinath ragalla <srinathragalla17@gmail.com>
2022-01-22 16:31:02 +0530

    edited js prjct file

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var random_margin = ["-5px", "1px", "5px", "10px", "7px"];
 var random_colors = ["#c2ff3d","#ff3de8","#3dc2ff","#04e022","#bc83e6","#ebb328"];
-var random_degree = ["rotate(3deg)", "rotate(1deg)", "rotate(-1deg)", "rotate(-3deg)", "rotate(-5deg)", "rotate(-8deg)"];
+var random_degree = ["rotate(3deg)", "rotate(-5deg)", "rotate(-1deg)", "rotate(3deg)", "rotate(-5deg)", "rotate(-8deg)"];
 var index = 0;
 
 window.onload = document.querySelector("#user_input").select();
